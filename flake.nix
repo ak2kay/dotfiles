35f20252c4283d27ed7476e3fbae764694d6a035
isvk <isregistermail@gmail.com>
2024-03-10 09:26:44 -0300

    change: use new user name

--- flake.nix
+++ flake.nix
@@ -10,22 +10,22 @@
     ags.url = "github:Aylur/ags";
     spicetify-nix.url = "github:the-argus/spicetify-nix";
     datsnvim = {
-      url = "git+file:///home/nixos/.dotfiles/dotfiles/nvim?shallow=1";
+      url = "git+file:///home/isvk/.dotfiles/dotfiles/nvim?shallow=1";
       flake = false;
     };
     walls = {
-      url = "git+file:///home/nixos/.dotfiles/modules/walls?shallow=1";
+      url = "git+file:///home/isvk/.dotfiles/modules/walls?shallow=1";
       flake = false;
     };
     unix-scripts = {
-      url = "git+file:///home/nixos/.dotfiles/dotfiles/bin?shallow=1";
+      url = "git+file:///home/isvk/.dotfiles/dotfiles/bin?shallow=1";
       flake = false;
     };
   };
 
   outputs = inputs@{ self, nixpkgs, home-manager, ... }:
     let
-      vars = { user = "dtsf"; };
+      vars = { user = "isvk"; };
     in {
       nixosConfigurations = (
         import ./core {
