b3a59144a3f66dbf8689e2802d834d5130ed1f83
ak2kay <isregistermail@gmail.com>
2024-03-12 18:38:25 +0800

    fix(script): fix lint and format

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 git_root=$(git rev-parse --show-toplevel)
 
 if [ "$PWD" != "$git_root" ]; then
-  echo "please run this script from the root of the git repo"
-  exit 1
+	echo "please run this script from the root of the git repo"
+	exit 1
 fi
 
 # update config submodules
@@ -16,10 +16,10 @@ sudo nixos-rebuild switch --impure --flake .#dtsf-machine
 
 # update ags dependencies
 ags_root="$HOME/.config/ags"
-pushd $ags_root || exit 1
+pushd "$ags_root" || exit 1
 
 if [ ! -d "$ags_root/node_modules" ]; then
-  direnv exec "$HOME/.config/ags" pnpm i
+	direnv exec "$HOME/.config/ags" pnpm i
 fi
 
 bat cache --build
