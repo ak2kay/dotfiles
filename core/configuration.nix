6b01fe480f028ce621fb95aa3b5f425a9efc8f52
isvk <isregistermail@gmail.com>
2024-05-14 23:36:16 +0800

    change: refine 1password

--- core/configuration.nix
+++ core/configuration.nix
@@ -42,6 +42,24 @@
     ignoreShellProgramCheck = true;
   };
 
+  programs._1password.enable = true;
+  programs._1password-gui = {
+    enable = true;
+    # Certain features, including CLI integration and system authentication support,
+    # require enabling PolKit integration on some desktop environments (e.g. Plasma).
+    polkitPolicyOwners = [ "${vars.user}" ];
+  };
+
+  environment.etc = {
+    "1password/custom_allowed_browsers" = {
+      text = ''
+        chromium
+        firefox
+      '';
+      mode = "0755";
+    };
+  };
+
   fonts = {
     fontDir.enable = true;
     packages = with pkgs; [
--- home/apps/1password.nix
+++ home/apps/1password.nix
@@ -1,26 +1,10 @@
 { config, lib, vars, pkgs, ... }:
 {
-  programs._1password.enable = true;
-  programs._1password-gui = {
-    enable = true;
-    # Certain features, including CLI integration and system authentication support,
-    # require enabling PolKit integration on some desktop environments (e.g. Plasma).
-    polkitPolicyOwners = [ "${vars.user}" ];
-  };
   programs.ssh = {
     enable = true;
     extraConfig = ''
       Host *
           IdentityAgent ~/.1password/agent.sock
     '';
-  };
-  environment.etc = {
-    "1password/custom_allowed_browsers" = {
-      text = ''
-        chromium
-        firefox
-      '';
-      mode = "0755";
-    };
   };
 }
